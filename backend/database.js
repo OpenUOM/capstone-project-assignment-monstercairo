81c69c1ed85d83e5bfea5cc9dc2c39d8b7b8112d
nufail <ahnufail@gmail.com>
2024-03-08 10:15:59 +0530

    database function changes

--- backend/database.js
+++ backend/database.js
@@ -124,7 +124,7 @@ const addStudent = async (name, age, religion, id) => {
   const sql = `INSERT INTO Student SET name=?, age=?, religion=? WHERE id=?`;
   return new Promise((resolve, reject) => {
     knex_db
-      .raw(sql, [id, name, age, religion])
+      .raw(sql, [name, age, religion, id])
       .then(() => {
         resolve({ status: "Successfully inserted Student" });
       })
